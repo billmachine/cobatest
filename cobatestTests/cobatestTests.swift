a1e293cdf666d1c451ebf114b7e7b2366457f3cf
billi <ahmad.billi@nostratech.com>
2021-02-05 06:02:36 +0700

    adding testing

--- cobatestTests/cobatestTests.swift
+++ cobatestTests/cobatestTests.swift
@@ -8,26 +8,26 @@
 import XCTest
 @testable import cobatest
 
-class cobatestTests: XCTestCase {
+class OfflineAppTestTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testInvalidInputSides() {
+        XCTAssertThrowsError(try detectTriangle(-1, 2, 3)) { error in
+            XCTAssertEqual(error as? TriangleError, TriangleError.invalidInput)
         }
     }
+    
+    func detectTriangle(
+        _ sideA: Int,
+        _ sideB: Int,
+        _ sideC: Int
+    ) throws {
+        if sideA < 1 {
+            throw TriangleError.invalidInput
+        }
+    }
+    
+}
 
+enum TriangleError: Error {
+    case invalidInput
 }
